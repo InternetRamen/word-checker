fdd040c3d759759f0c8cbcfd6bb04fb09dba257e
Jaden <66806100+InternetRamen@users.noreply.github.com>
2022-02-15 14:17:58 -0500

    fixed bug

--- find.js
+++ find.js
@@ -3,14 +3,20 @@ const words = require('./words.json')
 fs.readFile('./draft.txt', 'utf8', function(err, data) {
   if (err) throw err;
   let noWords = words.value
-  let txtArray = data.split(" ")
-  for (let singleWord of noWords) {
-	  for (let txt of txtArray) {
-		  if (txt === singleWord) {
-			  console.log(`You have ${singleWord} in the story!`)
-		  }
-	  }
+  let lines = data.split("\n")
+  let x = 0;
+  for (let line of lines) {
+    let txtArray = line.split(" ")
+    for (let singleWord of noWords) {
+      for (let txt of txtArray) {
+        if (txt.trim() === singleWord) {
+          console.log(`You have ${singleWord} in the story!`)
+          x++;
+        }
+      }
+    }
   }
-  if (txtArray.some(val => noWords.includes(val))) return console.log("YOU DIDN'T PASS")
+ 
+  if (x != 0) return console.log("YOU DIDN'T PASS")
   console.log("YAY YOU PASSED!")
 });
